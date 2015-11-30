e82c2dc29b39001cb75b0f40970476b8bace7979
Thomas Jansen <t.jansen@sap.com>
2015-11-30 08:41:26 +0100

    separate build and compile step, plus dist folder

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,20 @@
 var gulp = require('gulp');
 var tslint = require('gulp-tslint');
+var tsc = require('gulp-tsc');
 
 gulp.task('tslint', function(){
       return gulp.src('src/**/*.ts')
         .pipe(tslint())
         .pipe(tslint.report('verbose'));
+});
+
+gulp.task('build', ['compile'], function () {
+    return gulp.src('./src/**/*.html')
+        .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('compile', function(){
+    gulp.src(['src/**/*.ts'])
+        .pipe(tsc(require('./src/tsconfig.json').compilerOptions))
+        .pipe(gulp.dest('./dist'));
 });
